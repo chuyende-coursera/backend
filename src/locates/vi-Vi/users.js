b822f9abbe69494a32e4ffef53745c97754cbbc1
Ho Quoc Huy <huy.hoquoc99@gmail.com>
2021-04-08 22:37:04 +0700

    Add service helper api

---
+++ src/locates/vi-Vi/users.js
@@ -0,0 +1,14 @@
+export default {
+	'api.users.id': 'Id tài khoản',
+	'api.users.telephone': 'Số điện thoại',
+	'api.users.password': 'Mật khẩu',
+	'api.users.oldPassword': 'Mật khẩu cũ',
+	'api.users.newPassword': 'Mật khẩu mới',
+	'api.users.newPasswordConfirm': 'Mật khẩu mới xác thực',
+	'api.users.name': 'Tên đầy đủ',
+	'api.users.uuid': 'Mã thiết bị',
+	'api.users.avatar': 'Ảnh đại diện',
+	'api.users.avatarCover': 'Ảnh bìa',
+	'api.users.countFriends': 'Số lương bạn bè',
+	'api.users.birthday': 'Ngày sinh',
+};
